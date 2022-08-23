f0f5b38d8319348bf8d823d3bd10f7386425b5bc
ismael-soler <ismaelsolerferreira@gmail.com>
2022-08-23 16:31:22 -0300

    betty v3

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,9 +1,10 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sorts an array of integers in ascending order.
- * @array: array of integrers
- * @size: size of the array
+ * bubble_sort - sorts an array of integrers in ascending order
+ *
+ * @array: array to be sorted
+ * @size: size of @array
  */
 
 void bubble_sort(int *array, size_t size)
